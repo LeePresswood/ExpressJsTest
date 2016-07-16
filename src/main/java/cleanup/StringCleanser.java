0212510206833abd9505555912b8ec398352375e
Lee Presswood <LeePresswood@gmail.com>
2016-07-16 09:15:34 -0500

    Created StringCleanser.

---
+++ src/main/java/cleanup/StringCleanser.java
@@ -0,0 +1,43 @@
+package cleanup;
+
+/**
+ * Made by Lee on 7/16/2016.
+ */
+public class StringCleanser {
+    public static String cleanse(String string){
+        return stringIsEmpty(string) ? null : buildString(string);
+    }
+
+    private static String buildString(String string) {
+        StringBuilder builder = new StringBuilder();
+
+        for(char c : string.toCharArray()){
+            if(Character.isLetterOrDigit(c) || isMathPunctuation(c)){
+                builder.append(c);
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private static boolean stringIsEmpty(String string) {
+        return string == null || string.length() == 0;
+    }
+
+    private static boolean isMathPunctuation(char c) {
+        switch (c){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '(':
+            case ')':
+            case '|':
+            case '&':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
---
+++ src/test/java/cleanup/StringCleanserTest.java
@@ -0,0 +1,53 @@
+package cleanup;
+
+import cleanup.StringCleanser;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Made by Lee on 7/16/2016.
+ */
+public class StringCleanserTest {
+    @Test
+    public void testThatNullReturnsNull(){
+        String stringToCleanse = null;
+        assertEquals(null, StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatEmptyStringReturnsNull(){
+        String stringToCleanse = "";
+        assertEquals(null, StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatFrontSpacesAreRemoved(){
+        String stringToCleanse = "   Hello";
+        assertEquals("Hello", StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatBackSpacesAreRemoved(){
+        String stringToCleanse = "Goodbye    ";
+        assertEquals("Goodbye", StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatInnerSpacesAreRemoved(){
+        String stringToCleanse = "Hello      And   Goodbye";
+        assertEquals("HelloAndGoodbye", StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatNonMathPunctuationMarksAreRemoved(){
+        String stringToCleanse = "These Are Bad @#$%,<>.?_\"\\";
+        assertEquals("TheseAreBad", StringCleanser.cleanse(stringToCleanse));
+    }
+
+    @Test
+    public void testThatMathPunctuationMarksAreKept(){
+        String stringToCleanse = "These Are Good +-*/^()|&";
+        assertEquals("TheseAreGood+-*/^()|&", StringCleanser.cleanse(stringToCleanse));
+    }
+}
